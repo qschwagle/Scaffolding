1ba451dbd3e725ba72eae467b05e91e67783f588
Quinton Schwagle <qschwagle@gmail.com>
2020-08-19 21:28:30 -0400

    swapped IncludeMacro to using  over <>

--- src/IncludeMacro.cpp
+++ src/IncludeMacro.cpp
@@ -2,6 +2,6 @@
 
 void IncludeMacro::Write(std::ostream& out, const std::string& spaces)
 {
-    out << "#include <" << mPath << ">";
+    out << "#include \"" << mPath << "\"";
 }
 // vim:set sw=4 ts=4 et:
